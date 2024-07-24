b1e66f2991a4358186110b08fb8b943aa3cca90b
sammy <s21a23k@gmail.com>
2024-07-23 21:04:46 -0500

    move zeros to the end of the array

--- leetcode/src/main/java/org/example/MoveZeroes.java
+++ leetcode/src/main/java/org/example/MoveZeroes.java
@@ -1,2 +1,26 @@
-package org.example;public class MoveZeroes {
+package org.example;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+       int nonZeroIndex = 0;
+       for (int i=0;i<nums.length;i++){
+           if(nums[i] != 0){
+               nums[nonZeroIndex] = nums[i];
+               nonZeroIndex++;
+           }
+       }
+       while(nonZeroIndex < nums.length){
+           nums[nonZeroIndex] = 0;
+           nonZeroIndex++;
+       }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,3,0,12, 18, 9, 4};
+        moveZeroes(nums);
+        for(int i: nums){
+            System.out.print(i);
+        }
+    }
 }
